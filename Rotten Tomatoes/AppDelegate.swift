0883d5cff9b04a74e5dfd654366c43eb7cbe9af1
Vicki Grospe <vickichun@gmail.com>
2015-09-20 14:20:57 -0700

    move the NSURLCache setup to didFinishLaunchingWithOptions

--- Rotten Tomatoes/AppDelegate.swift
+++ Rotten Tomatoes/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let URLCache = NSURLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 20 * 1024 * 1024,
+            diskPath: "cachedResponse")
+        NSURLCache.setSharedURLCache(URLCache)
         return true
     }
 
@@ -33,9 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        let URLCache = NSURLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 20 * 1024 * 1024,
-            diskPath: "cachedResponse")
-        NSURLCache.setSharedURLCache(URLCache)
+       
     }
 
     func applicationWillTerminate(application: UIApplication) {
